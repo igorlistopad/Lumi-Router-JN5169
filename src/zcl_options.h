5c14973951810d98fcb52c05992dfe313bb51c84
Igor Listopad <31544843+igorlistopad@users.noreply.github.com>
2026-08-25 03:52:34 +0300

    fix: correct ZCL configuration

--- src/zcl_options.h
+++ src/zcl_options.h
@@ -8,30 +8,22 @@
 
 #include <jendefs.h>
 
-/**
- * @brief ZCL Specific initialization
- */
+/* Use the NXP manufacturer code */
 #define ZCL_MANUFACTURER_CODE 0x1037
 
 /* Number of endpoints supported by this device */
 #define ZCL_NUMBER_OF_ENDPOINTS 1
-
-/* ZCL has all cooperative tasks */
-#define COOPERATIVE
 
 /* Set this True to disable non-error default responses from clusters */
 #define ZCL_DISABLE_DEFAULT_RESPONSES (TRUE)
 
 /* Which custom commands need to be supported */
 #define ZCL_ATTRIBUTE_READ_SERVER_SUPPORTED
-#define ZCL_ATTRIBUTE_WRITE_SERVER_SUPPORTED
 
 /* Configuring Attribute Reporting */
 #define ZCL_ATTRIBUTE_REPORTING_SERVER_SUPPORTED
 #define ZCL_CONFIGURE_ATTRIBUTE_REPORTING_SERVER_SUPPORTED
 #define ZCL_READ_ATTRIBUTE_REPORTING_CONFIGURATION_SERVER_SUPPORTED
-#define ZCL_SYSTEM_MIN_REPORT_INTERVAL 0
-#define ZCL_SYSTEM_MAX_REPORT_INTERVAL 60
 
 /* Reporting related configuration */
 enum {
@@ -50,38 +42,27 @@ enum {
 /* Enable wild card profile */
 #define ZCL_ALLOW_WILD_CARD_PROFILE
 
-/**
- * @brief Enable Cluster
- * @note  Enables clusters and their client or server instances
- */
+/* Enable ZCL clusters and their client/server roles */
 #define CLD_BASIC
 #define BASIC_SERVER
+#define CLD_DEVICE_TEMPERATURE_CONFIGURATION
 #define DEVICE_TEMPERATURE_CONFIGURATION_SERVER
 
-/**
- * @brief Basic Cluster
- * @note  Optional Attributes
- */
-#define CLD_BAS_ATTR_APPLICATION_VERSION
-#define CLD_BAS_ATTR_STACK_VERSION
-#define CLD_BAS_ATTR_HARDWARE_VERSION
+/* Basic cluster optional attributes */
 #define CLD_BAS_ATTR_MANUFACTURER_NAME
 #define CLD_BAS_ATTR_MODEL_IDENTIFIER
 #define CLD_BAS_ATTR_DATE_CODE
 #define CLD_BAS_ATTR_SW_BUILD_ID
 
-#define BAS_MANUF_NAME_STRING "NXP"
+#define BAS_MANUF_NAME_STRING "OPENLUMI"
 #define BAS_MODEL_ID_STRING   "openlumi.gw_router.jn5169"
 #define BAS_DATE_STRING       BUILD_DATE_STRING
 #define BAS_SW_BUILD_STRING   "1000-0001"
 
-#define CLD_BAS_APP_VERSION      (1)
-#define CLD_BAS_STACK_VERSION    (1)
-#define CLD_BAS_HARDWARE_VERSION (1)
-#define CLD_BAS_MANUF_NAME_SIZE  (3)
-#define CLD_BAS_MODEL_ID_SIZE    (25)
-#define CLD_BAS_DATE_SIZE        (8)
+#define CLD_BAS_MANUF_NAME_SIZE  (sizeof(BAS_MANUF_NAME_STRING) - 1U)
+#define CLD_BAS_MODEL_ID_SIZE    (sizeof(BAS_MODEL_ID_STRING) - 1U)
+#define CLD_BAS_DATE_SIZE        (sizeof(BAS_DATE_STRING) - 1U)
+#define CLD_BAS_SW_BUILD_SIZE    (sizeof(BAS_SW_BUILD_STRING) - 1U)
 #define CLD_BAS_POWER_SOURCE     E_CLD_BAS_PS_SINGLE_PHASE_MAINS
-#define CLD_BAS_SW_BUILD_SIZE    (9)
 
 #endif /* ZCL_OPTIONS_H */
